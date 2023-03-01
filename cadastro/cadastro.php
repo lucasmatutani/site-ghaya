7fbeda2a78a8a4dddca19db028fc45a3375929f7
lucasmatutani <lucasmatutani@gmail.com>
2023-03-01 16:13:27 -0300

    hotfix

--- cadastro/cadastro.php
+++ cadastro/cadastro.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="./style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="/js/jquery.maskedinput.min.js" type="text/javascript"></script>
     <title>Cadastro</title>
 </head>
 
