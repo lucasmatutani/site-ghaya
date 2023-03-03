e66edb576d520695eb940dc9425a7316855005a2
lucasmatutani <lucasmatutani@gmail.com>
2023-03-02 22:44:25 -0300

    hotfix

--- cadastro/cadastro.php
+++ cadastro/cadastro.php
@@ -41,7 +41,7 @@
                 </div>
                 <div class="col-4">
                     <p style="margin: 0 0 1rem 0;">Anúncio</p>
-                    <select class="form-control form-select" aria-label="Default select example" name="tipo_anuncio" required>
+                    <select class="form-control form-select-sm" aria-label="Default select example" name="tipo_anuncio" required>
                         <option value="1">STANDARD</option>
                         <option value="2">PREMIUM</option>
                         <option value="3">SUPER_PREMIUM</option>
