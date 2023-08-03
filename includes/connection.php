878a91d16206a40dc9f84c94069c95710712b37f
lucasmatutani <lucasmatutani@gmail.com>
2023-08-02 23:27:52 -0300

    hotfix

--- includes/connection.php
+++ includes/connection.php
@@ -1,17 +1,9 @@
 <?php
-$url = explode("/", "$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]");
-$url_split = $url[0];
-if ($url_split == "localhost") {
-    $hostname = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "site_ghaya";
-} else {
-    $hostname = "localhost";
-    $username = "u680727787_matutani";
-    $password = "1dIpW35!5f9^";
-    $dbname = "u680727787_ghaya";
-}
+$hostname = "localhost";
+$username = "u680727787_matutani";
+$password = "1dIpW35!5f9^";
+$dbname = "u680727787_ghaya";
+
 
 // Create connection
 $conn = mysqli_connect($hostname, $username, $password, $dbname);
